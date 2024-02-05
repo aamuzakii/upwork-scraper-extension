f1aa4f7c5234778346759d829c8989b2eb05a110
Muzaki新联想 <aamuzakii@gmail.com>
2024-02-05 08:34:34 +0700

    /home/zaki/freelance/chrome-extensions-samples/functional-samples/sample.page-redder/manifest.json /home/zaki/freelance/chrome-extensions-samples/functional-samples/sample.page-redder/service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,12 @@
 function reddenPage() {
-  document.body.style.backgroundColor = 'blue';
-  chrome.action.setBadgeText({
-    text: "OFF",
-  });
+  document.body.style.backgroundColor = "blue";
 }
 
 chrome.action.onClicked.addListener((tab) => {
-  if (!tab.url.includes('chrome://')) {
+  if (!tab.url.includes("chrome://")) {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      function: reddenPage
+      function: reddenPage,
     });
   }
 });
