7f927b49323016797516e9bd614d8c3c958d8da0
Muzaki新联想 <aamuzakii@gmail.com>
2024-02-05 17:28:48 +0700

    copy func to searchUtl

--- service-worker.js
+++ service-worker.js
@@ -72,7 +72,76 @@ function reddenPage() {
 }
 
 function searchUtil() {
-  console.log(123);
+  const jobTileListElement = document.querySelector(
+    '[data-test="job-tile-list"]'
+  );
+
+  const arrOfJobs = [];
+
+  Array.from(jobTileListElement.children).forEach((section, i) => {
+    console.log("init", i);
+    const date =
+      section.children[0].children[1].children[0].children[0].innerHTML.replace(
+        /\s+/g,
+        " "
+      );
+
+    const title =
+      section.children[0].children[1].children[1].textContent.replace(
+        /\s+/g,
+        " "
+      );
+
+    const link = section.children[0].children[1].children[1].children[0].href
+      .replace(/\s+/g, " ")
+      .split("?")[0];
+
+    const content = section.children[1];
+
+    const fee = content.children[0];
+    const desc =
+      content.children[1].children[0].children[0].children[0].textContent.replace(
+        /\s+/g,
+        " "
+      );
+    const fofo = content.children[2].children;
+
+    const skill = fofo[0].children[0].children[2].children;
+
+    const skillCollection = [];
+
+    Array.from(skill).forEach((element) => {
+      skillCollection.push(element.textContent);
+    });
+
+    const applier = content.children[4].children[0].children[1].innerHTML;
+
+    let countryPar = content.children[3].children[3];
+    country = countryPar ? countryPar.textContent : "";
+
+    country = country.replace(/\s/g, "");
+
+    const newData = {
+      url: link,
+      title,
+      stack: JSON.stringify(skillCollection),
+      country,
+      candidates: applier,
+      description: desc,
+      date,
+    };
+
+    arrOfJobs.push(newData);
+  });
+
+  console.log(arrOfJobs);
+  fetch("http://localhost:3000/store", {
+    method: "POST",
+    // headers: {
+    //   "Content-Type": "application/json",
+    // },
+    body: JSON.stringify(arrOfJobs),
+  });
 }
 
 chrome.action.onClicked.addListener((tab) => {
